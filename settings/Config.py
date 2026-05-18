8f9e14d6621780bf55f4a763eea7601fda41479c
yuyuber12 <yonigames12@gmail.com>
2026-05-18 13:23:35 +0300

    changed font
    changed font

--- settings/Config.py
+++ settings/Config.py
@@ -15,8 +15,8 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 # Load shared fonts and UI assets used across screens.
-CURRENT_FONT = pygame.font.Font("Fonts/Grandboom Demo.otf", 45)
-CURRENT_FONT_BIGGER = pygame.font.Font("Fonts/Grandboom Demo.otf", 75)
+CURRENT_FONT = pygame.font.Font("Fonts/BlankRiver.ttf", 45)
+CURRENT_FONT_BIGGER = pygame.font.Font("Fonts/BlankRiver.ttf", 75)
 FINGER_IMAGE = pygame.image.load("Menu_images/finger-selector.png")
 FINGER_IMAGE = pygame.transform.scale(
     FINGER_IMAGE, ((WIDTH // 5 - 75), (HEIGHT // 5 - 50)))
